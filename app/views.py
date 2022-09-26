8e98c8190f639511efd9d177ce744135b124fae0
Almukhan Chingiz <chingizpain@gmail.com>
2022-09-26 23:47:04 +0600

    Был создан метод для начальной страницы

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from app.models import Product
+
+
+def index_view(request):
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'main_page.html', context)
