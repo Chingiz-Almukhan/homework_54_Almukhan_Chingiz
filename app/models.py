09fd8d598904725642d4a4dc06de062679bb5db0
Almukhan Chingiz <chingizpain@gmail.com>
2022-09-26 23:15:02 +0600

    Были отредактированы модели

--- app/models.py
+++ app/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(verbose_name='Название', max_length=200, null=False, blank=False)
-    description = models.CharField(verbose_name='Описание', max_length=15, null=True, blank=True)
+    description = models.CharField(verbose_name='Описание', max_length=200, null=True, blank=True)
     category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
     created_at = models.DateTimeField(verbose_name='Дата добавления', auto_now_add=True)
-    cost = models.DecimalField(verbose_name='Цена', max_digits=5, decimal_places=2)
-    image = models.CharField(verbose_name='Название', max_length=200, null=False, blank=False)
+    cost = models.DecimalField(verbose_name='Цена', max_digits=15, decimal_places=0)
+    image = models.CharField(verbose_name='Изображение', max_length=200, null=False, blank=False)
 
     def __str__(self):
         return f'{self.name} - {self.cost}'
