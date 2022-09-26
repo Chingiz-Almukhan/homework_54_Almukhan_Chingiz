58c85a82dca58a0ab6d6dc21fc70a51928320068
Almukhan Chingiz <chingizpain@gmail.com>
2022-09-27 04:23:15 +0600

    Были созданы методы для добавления товара/категории Также был прописан путь к ним

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from app.views import index_view, product_view
+from app.views import index_view, product_view, add_view, add, category_view, confirm_add
 
 urlpatterns = [
     path('', index_view, name='main'),
     path('products/<int:pk>', product_view, name='product'),
+    path('products/add', add_view, name='add'),
+    path('products/confirm/', add),
+    path('categories/add', category_view, name='category_add'),
+    path('categories/confirm/', confirm_add),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from app.models import Product
+from app.models import Product, Category
 
 
 def index_view(request):
@@ -16,3 +16,36 @@ def product_view(request, pk):
     return render(request, 'product_view.html', context={
         'product': product
     })
+
+
+def add_view(request):
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, 'add_product.html', context)
+
+
+def add(request):
+    name = request.POST.get('name')
+    description = request.POST.get('description')
+    cost = request.POST.get('cost')
+    image = request.POST.get('image')
+    category = request.POST.get('category')
+    product = Product.objects.create(name=name, description=description, cost=cost, image=image, category_id=category)
+    return redirect('product', pk=product.pk)
+
+
+def category_view(request):
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, 'add_category.html', context)
+
+
+def confirm_add(request):
+    name = request.POST.get('name')
+    description = request.POST.get('description')
+    category = Category.objects.create(name=name, description=description)
+    return redirect('main')
