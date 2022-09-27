d8119f0aaf12303c476839cf4c93ec807f99bf84
Almukhan Chingiz <chingizpain@gmail.com>
2022-09-27 06:54:50 +0600

    Отредактировал пути в связи с тем, что оптимизировал методы

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from app.views import index_view, product_view, add_view, add, category_view, confirm_add
+from app.views import index_view, product_view, add_view, category_view, edit_view, delete_view
 
 urlpatterns = [
     path('', index_view, name='main'),
     path('products/<int:pk>', product_view, name='product'),
     path('products/add', add_view, name='add'),
-    path('products/confirm/', add),
     path('categories/add', category_view, name='category_add'),
-    path('categories/confirm/', confirm_add),
+    path('products/<int:pk>/edit', edit_view, name='edit'),
+    path('products/<int:pk>/delete', delete_view, name='delete'),
 
 ]
