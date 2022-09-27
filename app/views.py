9ee76bd605f61194c8e6d862271ca6ec2f3e090d
Almukhan Chingiz <chingizpain@gmail.com>
2022-09-27 06:55:13 +0600

    Создал методы для редактирования и удаления продукта

--- app/views.py
+++ app/views.py
@@ -19,33 +19,54 @@ def product_view(request, pk):
 
 
 def add_view(request):
-    categories = Category.objects.all()
-    context = {
-        'categories': categories
-    }
-    return render(request, 'add_product.html', context)
-
-
-def add(request):
-    name = request.POST.get('name')
-    description = request.POST.get('description')
-    cost = request.POST.get('cost')
-    image = request.POST.get('image')
-    category = request.POST.get('category')
-    product = Product.objects.create(name=name, description=description, cost=cost, image=image, category_id=category)
-    return redirect('product', pk=product.pk)
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        context = {
+            'categories': categories
+        }
+        return render(request, 'add_product.html', context)
+    elif request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        cost = request.POST.get('cost')
+        image = request.POST.get('image')
+        category = request.POST.get('category')
+        product = Product.objects.create(name=name, description=description, cost=cost, image=image,
+                                         category_id=category)
+        return redirect('product', pk=product.pk)
 
 
 def category_view(request):
-    categories = Category.objects.all()
-    context = {
-        'categories': categories
-    }
-    return render(request, 'add_category.html', context)
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        context = {
+            'categories': categories
+        }
+        return render(request, 'add_category.html', context)
+    elif request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        category = Category.objects.create(name=name, description=description)
+        return redirect('main')
 
 
-def confirm_add(request):
-    name = request.POST.get('name')
-    description = request.POST.get('description')
-    category = Category.objects.create(name=name, description=description)
+def edit_view(request, pk):
+    product = Product.objects.get(pk=pk)
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        context = {'product': product,
+                   'categories': categories}
+        return render(request, 'edit_view.html', context)
+    elif request.method == 'POST':
+        product.name = request.POST.get('name')
+        product.description = request.POST.get('description')
+        product.cost = request.POST.get('cost')
+        product.image = request.POST.get('image')
+        product.save()
+        return redirect('main')
+
+
+def delete_view(request, pk):
+    product = Product.objects.get(pk=pk)
+    product.delete()
     return redirect('main')
